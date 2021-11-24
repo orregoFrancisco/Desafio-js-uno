7972b37ed9b8f0d0c956ca404ef0846e6bcee654
orrego <francisco.j.orrego@gmail.com>
2021-11-24 08:11:57 -0300

    script dias semana año es modificado

--- script/diaSemanaAño.js
+++ script/diaSemanaAño.js
@@ -1,16 +1,44 @@
 //diaSemanaAño
 
 
-
 let dias =parseInt(prompt("cuantos dias" ));
+let año=365
+let semana=7
+let resultado=dias/año
+let resultadob=dias/semana
+let horas = dias*24
+
+document.write("la cantidad de años son  "+Math.floor(resultado)+"<br>");
+document.write("la cantidad de semanas son "+Math.floor(resultadob)+"<br>");
+document.write("La cantidad de dias son "+ dias+"<br>");
+document.write("la cantidad horas son "+Math.floor(horas)+"<br><br><br><br>");
+
+
+
+let diasB =parseInt(prompt("cuantos dias" ));
+if(dias < 365){
+    let operacionSemana = diasB/7;
+    document.write("la semana /"+Math.floor(operacionSemana)+"<br>")
+    let operacionDias = diasB % 7;
+    document.write("el dia /"+Math.floor(operacionDias)+"<br><br>")
+}else{
+    let operacionSemana =(diasB - 365) / 7;
+    document.write("la semana /"+Math.floor(operacionSemana)+"<br>")
+    let operacionDias = (diasB - 365) - 7;
+    document.write("el dia /"+Math.floor(operacionDias)+"<br><br>")
+}
+
 let años = 365
-let semana = 7
-let operacionAño = años%dias
-document.write("el año /"+Math.floor(operacionAño)+"<br>")
-let operacionSemana = dias / 52;
-document.write("la semana /"+Math.floor(operacionSemana)+"<br>")
-let operacionDias = dias /años;
-document.write("el dia /"+Math.floor(operacionDias)+"<br><br>")
+if(dias < 365){
+    operacionAño = 0;
+    document.write("el año /"+Math.floor(operacionAño)+"<br>")
+}else{
+    let operacionAño = años%diasB
+    document.write("el año /"+Math.floor(operacionAño)+"<br>")
+}
+
+
+
 
 
 
