08d2be93db583dbc23ab69d36b25d6d9319f90ec
orrego <francisco.j.orrego@gmail.com>
2021-11-23 21:23:40 -0300

    se modifica el Y entre num

--- script/operacionesFlujo.js
+++ script/operacionesFlujo.js
@@ -4,33 +4,33 @@ const operaciones = (num1,num2) => {
     let preguntaB = true
     if(preguntaB){
         let aplicar = num1 + num2;
-        console.log(`numeros operados ${num1} y ${num2}`)
+        console.log(`numeros operados ${num1} y ${num2}`);
         console.log("resultado suma "+aplicar);
-        alert(`El resultado de la suma es ${aplicar}`)
+        alert(`El resultado de la suma es ${aplicar}`);
     }
     if(preguntaB){
         let aplicar = num1 - num2;
-        console.log(`numeros operados ${num1} y ${num2}`)
+        console.log(`numeros operados ${num1} y ${num2}`);
          console.log("resultado resta "+ aplicar);
-         alert(`El resultado de la resta es ${aplicar}`)
+         alert(`El resultado de la resta es ${aplicar}`);
     }
     if(preguntaB){
         let aplicar = num1 * num2;
-        console.log(`numeros operados ${num1} y ${num2}`)
+        console.log(`numeros operados ${num1} y ${num2}`);
          console.log("resultado multiplicacion "+ aplicar);
-         alert(`El resultado de la multiplicacion es ${aplicar}`)
+         alert(`El resultado de la multiplicacion es ${aplicar}`);
     }
     if(preguntaB){
         let aplicar = num1 / num2;
-        console.log(`numeros operados ${num1} y ${num2}`)
+        console.log(`numeros operados ${num1} y ${num2}`);
          console.log("resultado division "+ aplicar);
-         alert(`El resultado de la division es ${aplicar}`)
+         alert(`El resultado de la division es ${aplicar}`);
     }
     if(preguntaB){
         let aplicar = num1 % num2;
-        console.log(`numeros operados ${num1} y ${num2}`)
+        console.log(`numeros operados ${num1} y ${num2}`);
          console.log("resultado residuo "+ aplicar);
-         alert(`El resultado del residuo es ${aplicar}`)
+         alert(`El resultado del residuo es ${aplicar}`);
     }
     
 };
@@ -43,5 +43,5 @@ if(A == B || B==A || A < 0 || B < 0){
     alert("Los numeros deben ser distintos y mayores a cero");
 }else{
     operaciones(A,B);
-}
+};
 
