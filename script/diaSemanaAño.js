c35ede49e8156bd8b8a3df1746322046e90cdb74
orrego <francisco.j.orrego@gmail.com>
2021-11-23 23:34:45 -0300

    se modifica el jv de dias,semana,año

--- script/diaSemanaAño.js
+++ script/diaSemanaAño.js
@@ -1,13 +1,17 @@
 //diaSemanaAño
 
-let dias =parseInt(prompt("cuantos dias" ));
-let año=365
-let semana=7
-let resultado=dias/año
-let resultadob=dias/semana
-let horas = dias*24
 
-document.write("la cantidad de años son  "+Math.floor(resultado)+"<br>");
-document.write("la cantidad de semanas son "+Math.floor(resultadob)+"<br>");
-document.write("La cantidad de dias son "+ dias+"<br>");
-document.write("la cantidad horas son "+Math.floor(horas)+"<br>");
+
+let dias =parseInt(prompt("cuantos dias" ));
+let años = 365
+let semana = 7
+let operacionAño = años%dias
+document.write("el año /"+Math.floor(operacionAño)+"<br>")
+let operacionSemana = dias / 52;
+document.write("la semana /"+Math.floor(operacionSemana)+"<br>")
+let operacionDias = dias /años;
+document.write("el dia /"+Math.floor(operacionDias)+"<br><br>")
+
+
+
+
